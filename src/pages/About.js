76ebcf684e90c6b3c6539cd73a337e5d03810ad2
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-02-17 17:13:34 +0100

    react flags

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla animi reiciendis aperiam dolorum inventore quisquam dicta deleniti, odit provident optio quis aliquam rem quaerat eos ut autem facilis? Recusandae, laudantium.
+        blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  blibloblu  bliblobluh
+      </p>
+    </div>
+  );
+};
+
+export default About;
